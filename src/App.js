00e017295d9116286b8155f475930f5b9a2a6589
Sebastian Grzelak <segrz8@gmail.com>
2020-03-30 14:06:31 +0200

    Add lorem to Main

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+
 import MainSlider from './Components/MainSlider'
+import Hamburger from './Components/Hamburger';
+import Menu from './Components/Menu';
+import Main from './Components/Main';
 
 class App extends React.Component {
 
-	// state = {  }
+	state = {
+		menuActive: false,
+	}
+
+	handleMenu = () => {
+		this.setState({
+			menuActive: !this.state.menuActive,
+		});
+	}
 
 	render() {
 		return (
 			<div className="App">
-				<MainSlider />
+				<BrowserRouter basename={process.env.PUBLIC_URL}>
+					<Hamburger
+						menuActive={this.state.menuActive}
+						handleMenu={this.handleMenu}
+					/>
+					<Menu
+						menuActive={this.state.menuActive}
+						handleMenu={this.handleMenu}
+					/>
+					<MainSlider />
+					<Main />
+				</BrowserRouter>
 			</div>
 		);
 	}
--- src/Components/MainSlider.js
+++ src/Components/MainSlider.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import './MainSlider.scss';
+import Carousel from 'react-bootstrap/Carousel';
 
-function MainSlider() {
+import img1 from '../img/beach.jpg';
+import img1s from '../img/beachS.jpg';
+import img2 from '../img/church.jpg';
+import img2s from '../img/churchS.jpg';
+
+const MainSlider = () => {
+
+    const imgSet1 = [
+        { path: img1, pathS: img1s, },
+        { path: img2, pathS: img2s, },
+    ]
+
+    const carouselItems1 = imgSet1.map(item => {
+        return (
+            <Carousel.Item key={item.path}>
+                <picture>
+                    <source media="(min-width: 461px)" srcSet={item.path} />
+                    <img src={item.pathS} className="d-block w-100" alt="..." />
+                </picture>
+            </Carousel.Item>
+        )
+    })
+
     return (
-        <div>
-            a
+        <div className="MainSlider">
+            <div className="MainSlider__carousel">
+                <Carousel controls={false} indicators={false}>
+                    {carouselItems1}
+                </Carousel>
+            </div>
         </div>
     )
 }
