bb2c65d2da33f8753d0b01989f60442ec015fbd6
Sebastian Grzelak <segrz8@gmail.com>
2020-03-30 20:16:19 +0200

    Updates

---
+++ precache-manifest.8e40d0ec759ae93ce2f4f8a8f435fe92.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52b73a81d3a3fbde0f52debdfb6b1e2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51ce3f0d2e21852329da",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "582ce96db11b910783b5",
+    "url": "/static/css/main.c7434edb.chunk.css"
+  },
+  {
+    "revision": "51ce3f0d2e21852329da",
+    "url": "/static/js/2.3d91ac81.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3d91ac81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582ce96db11b910783b5",
+    "url": "/static/js/main.2d44f5ac.chunk.js"
+  },
+  {
+    "revision": "a48e7f01e92efb3b1284",
+    "url": "/static/js/runtime-main.00c33afe.js"
+  },
+  {
+    "revision": "a00f526ae0224bfd7f23021596b1afaa",
+    "url": "/static/media/beach.a00f526a.jpg"
+  },
+  {
+    "revision": "8b13b5c526c16bdbd93fc5b5d3379d6a",
+    "url": "/static/media/beachS.8b13b5c5.jpg"
+  },
+  {
+    "revision": "4edc00e782aa4018319beaaf768b76b0",
+    "url": "/static/media/church.4edc00e7.jpg"
+  },
+  {
+    "revision": "002df4c6aceae183ab36057f05ffb470",
+    "url": "/static/media/churchS.002df4c6.jpg"
+  },
+  {
+    "revision": "693a8f82a3e04f5d69673197ecd72ad6",
+    "url": "/static/media/lake.693a8f82.jpg"
+  },
+  {
+    "revision": "58c319212957b5b7729c7c50e60237bc",
+    "url": "/static/media/lakeS.58c31921.jpg"
+  }
+]);
